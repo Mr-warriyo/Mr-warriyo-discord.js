c9825b7dd678c052bcecce09c2a257c13057c8a2
Mr-warriyo <78315967+Mr-warriyo@users.noreply.github.com>
2021-01-31 23:30:31 +0530

    Create index.js

---
+++ Ep 4 - How to make avatar command/index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+ 
+client.on('message', message => {
+  if (message.content === '${config.prefix}ping') {
+    message.channel.send(`The Ping Of Bot Is ${client.ws.ping}ms.\nBot Latency ${Date.now() - message.createdTimestamp}ms`);
+  }
+});
+
+
+client.on('message', message => {
+  if (message.content.startsWith('${config.prefix}avatar')) {
+    const user = message.mentions.users.first() || message.author;
+    const avEmbed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setAuthor(user.tag)
+        .setImage(user.avatarURL({dynamic: "true"}));
+    message.channel.send(avEmbed)
+  } 
+});
+
+client.login(config.token);
