80b47d54e0df6452a0040c578f07c964fd6878e4
Mr-warriyo <78315967+Mr-warriyo@users.noreply.github.com>
2021-01-31 23:23:12 +0530

    Create Index.js

---
+++ Ep 2 - How to make bot reply messages and commands/Index.js
@@ -0,0 +1,19 @@
+```
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity("Hi")
+});
+
+client.on('message', message => { 
+  if (message.content === '${config.prefix}ping') {
+    message.channel.send(pong);
+  } 
+});
+
+
+client.login("config.token")
+```
