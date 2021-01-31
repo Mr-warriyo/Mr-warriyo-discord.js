d18677047b34807845c154f27b4da9f0430a19a2
Mr-warriyo <78315967+Mr-warriyo@users.noreply.github.com>
2021-01-31 23:32:52 +0530

    Update index.js

--- Ep 4 - How to make avatar command/index.js
+++ Ep 4 - How to make avatar command/index.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json')
  
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on('message', message => {
   if (message.content === '${config.prefix}ping') {
     message.channel.send(`The Ping Of Bot Is ${client.ws.ping}ms.\nBot Latency ${Date.now() - message.createdTimestamp}ms`);
