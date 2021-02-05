4e4c36e9ce4f1badb792ce44437ccd144c8a349f
Mr-warriyo <78315967+Mr-warriyo@users.noreply.github.com>
2021-02-05 08:00:56 +0530

    Update Index.js

--- Ep 2 - How to make bot reply messages and commands/Index.js
+++ Ep 2 - How to make bot reply messages and commands/Index.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('../config.json')
+const config = require('./config.json')
 
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
